029e718f3489b42aa2be9bbe31b12a3772b6aef5
Bench <TheMineBench@gmail.com>
2015-05-08 14:20:38 -0400

    Signed-off-by: Bench <TheMineBench@gmail.com>

--- src/me/theminebench/onduty/OnDuty.java
+++ src/me/theminebench/onduty/OnDuty.java
@@ -35,24 +35,44 @@ public class OnDuty extends JavaPlugin {
 		}
 		command.setExecutor(new Buteti());
 	}
-	
+	/**
+	 * Gets the message broadcasted when someone goes on duty!
+	 * @return the enableDutyMessage
+	 */
 	public static String getEnableDutyMessage() {
 		return getPlugin().enableDutyMessage;
 	}
+	/**
+	 * Gets the message broadcasted when someone goes off duty!
+	 * @return the disableDutyMessage
+	 */
 	public static String getDisableDutyMessage() {
 		return getPlugin().disableDutyMessage;
 	}
+	
+	/**
+	 * Gets the message sent when someone checks to see if someone is on duty and they are!
+	 * @return the playersOnDutyMessage
+	 */
 	public static String getPlayersOnDutyMessage() {
 		return getPlugin().playersOnDutyMessage;
 	}
+	
+	/**
+	 * Gets the message sent when someone checks to see if someone is on duty and they arn't!
+	 * @return the playersOffDutyMessage
+	 */
 	public static String getPlayersOffDutyMessage() {
 		return getPlugin().playersOffDutyMessage;
 	}
+	
+	/**
+	 * Gets the message sent when someone checks to see if someone is on duty and the person is not found!
+	 * @return the playersOffDutyMessage
+	 */
 	public static String getUnableToFindPlayerMessage() {
 		return getPlugin().unableToFindPlayerMessage;
 	}
-	
-	
 	
 	public static OnDuty getPlugin() {
 		return plugin;
